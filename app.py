e2da3fddd8f1f4bf3d09af630767551d39ddb3db
mohamed <temsamanim840@gmail.com>
2026-05-12 06:07:37 +0100

    fix app

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from models.models_employee import Employee
 from models.models_medicine import Medicine
 from models.models_attendance import Attendance
 from models.models_task import Task
-from models.models_billing import Billing
 
-from services.services_patient import  get_all_patients
 from services.services_employee import add_employee_service, get_all_employees, get_employee_by_id, update_employee_service
 from services.services_medicine import add_medicine_service, get_all_medicines, get_medicine_by_id, update_medicine_service, delete_medicine_service
 from services.services_attendance import add_attendance_service, get_all_attendance, get_attendance_by_id, update_attendance_service, delete_attendance_service
 from services.services_task import add_task_service, get_all_tasks, get_task_by_id, update_task_service, delete_task_service
-from services.services_billing import add_billing_service, get_all_billings, get_billing_by_id, update_billing_service, delete_billing_service
 
 from routes.routes_patient import patient_bp
+from routes.routes_bed import bed_bp
+from routes.routes_billing import billing_bp
 
 
 
@@ -25,7 +24,8 @@ with app.app_context():
     db.create_all()
 
 app.register_blueprint(patient_bp)
-
+app.register_blueprint(bed_bp)
+app.register_blueprint(billing_bp)
 # ─── HOME ──────────────────────────────────────────────
 @app.route('/')
 def base():
@@ -139,34 +139,6 @@ def edit_task(id):
 def delete_task(id):
     delete_task_service(id)
     return redirect(url_for('view_update_task'))
-
-# ─── BILLING ───────────────────────────────────────────
-@app.route('/add_billing', methods=['GET', 'POST'])
-def add_billing():
-    if request.method == 'POST':
-        add_billing_service(request.form)
-        return redirect(url_for('view_update_billing'))
-    patients = get_all_patients()
-    return render_template("add_billing.html", patients=patients)
-
-@app.route('/view_update_billing')
-def view_update_billing():
-    bills = get_all_billings()
-    return render_template('view_update_billing.html', bills=bills)
-
-@app.route('/edit_billing/<int:id>', methods=['GET', 'POST'])
-def edit_billing(id):
-    bill = get_billing_by_id(id)
-    if request.method == 'POST':
-        update_billing_service(id, request.form)
-        return redirect(url_for('view_update_billing'))
-    patients = get_all_patients()
-    return render_template('edit_billing.html', bill=bill, patients=patients)
-
-@app.route('/delete_billing/<int:id>')
-def delete_billing(id):
-    delete_billing_service(id)
-    return redirect(url_for('view_update_billing'))
 
 if __name__ == '__main__':
     app.run(debug=True)
